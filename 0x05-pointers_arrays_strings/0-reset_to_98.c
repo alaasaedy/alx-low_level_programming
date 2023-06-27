079d84147daa2315dcb907864e0e50639521f60c
Alaa <alaa.saedy.eg@gmail.com>
2023-06-26 17:14:36 -0700

    function that takes a pointer to an int as parameter and updates the value it points to to 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -7,7 +7,7 @@
  * Return: n
  */
 
-int reset_to_98(int *n)
+void reset_to_98(int *n)
 {
 	*n = 98;
 }
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,7 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 #include <stdio.h>
-#include <unistd.io>
+#include <unistd.h>
 
 int _putchar(char c);
 void reset_to_98(int *n);
@@ -16,3 +17,4 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 
 #endif
+
